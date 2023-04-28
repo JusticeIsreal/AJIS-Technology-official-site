a19ba3c0d0fcd3080641e9640cf2605cdcad8df8
Justice Isreal <justiceyba@gmail.com>
2023-04-29 00:16:36 +0100

    team page responsiveness complete

---
+++ Components/home/Teamwork.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+function Teamwork() {
+  return (
+    <div className="teamwork">
+      <div className="lower-team-bg">
+        <h2>
+          If you can conceive it <br /> we can acheive it
+        </h2>
+        <div className="team-quote">
+          <p>our product is</p>
+          <h2>cool</h2>
+          <p>our process is</p>
+          <h2>simple</h2>
+          <p>the result is</p>
+          <h2>epic</h2>
+          <p>the customer is</p>
+          <h2>everything</h2>
+          <div>
+            <button>let's talk</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Teamwork;
--- pages/homepage.jsx
+++ pages/homepage.jsx
@@ -2,6 +2,7 @@ import Aboutus from "@/Components/home/Aboutus";
 import Banner from "@/Components/home/Banner";
 import Navbar from "@/Components/home/Navbar";
 import Services from "@/Components/home/Services";
+import Teamwork from "@/Components/home/Teamwork";
 import React from "react";
 
 function homepage() {
@@ -11,6 +12,7 @@ function homepage() {
       <Banner />
       <Aboutus />
       <Services />
+      <Teamwork />
     </div>
   );
 }
