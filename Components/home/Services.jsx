1123fe9c22af0ed26b08742005273d7c022fff71
Justice Isreal <justiceyba@gmail.com>
2023-04-28 23:19:41 +0100

    service page responsiveness complete

--- Components/home/Services.jsx
+++ Components/home/Services.jsx
@@ -1,30 +1,35 @@
+import Link from "next/link";
 import React from "react";
 
 function Services() {
   const Services = [
     {
       id: "1",
-      service: "mobile app",
+      service: "Mobile app",
       desc: "All-in-one mobile solution for digital business services",
       img: "/the-average-tech-guy-DsmDqiYduaU-unsplash.jpg",
+      link: "/mobileapp",
     },
     {
       id: "2",
-      service: "Product design",
+      service: "Ui / Ux",
       desc: "Stay top-of-mind with your audience with consistent digital designs.",
       img: "/alvaro-reyes-zvmZiw3vdsQ-unsplash.jpg",
+      link: "/uiux",
     },
     {
       id: "3",
       service: "Website",
       desc: "Custom websites to enhance your company's image.",
       img: "/igor-miske-JVSgcV8_vb4-unsplash.jpg",
+      link: "/website",
     },
     {
       id: "4",
-      service: "Design",
+      service: "Graphics",
       desc: "Our in-house team of professional designers can meet all of your design needs.",
       img: "/onur-binay-wi3xBnWZlvQ-unsplash.jpg",
+      link: "/design",
     },
   ];
 
@@ -44,10 +49,21 @@ function Services() {
 
 export default Services;
 
-function SingleServices({ id, service, desc, img }) {
+function SingleServices({ id, service, desc, img, link }) {
   return (
     <div className="card">
       <img src={img} alt="" />
+      <div className="service-txt">
+        <div className="desc">
+          <h2>{service}</h2>
+          <p>{desc}</p>
+          <Link href={link}>
+            <div className="card-btn">
+              <button>See more</button>
+            </div>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
