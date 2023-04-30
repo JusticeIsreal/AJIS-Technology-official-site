3fb715f452e112b24a8fa387a98ed170f5ef0d40
Justice Isreal <justiceyba@gmail.com>
2023-04-30 04:35:09 +0100

    web site page complete

--- Components/website/Services.jsx
+++ Components/website/Services.jsx
@@ -4,45 +4,35 @@ import React from "react";
 function Services() {
   const Services = [
     {
-      id: "1",
-      img: "/Screenshot 2023-04-30 at 01.31.39.png",
-    },
-    {
       id: "2",
       service: "E-Commerce",
-      desc: "Stay top-of-mind with your audience with consistent digital designs.",
+      img: "/Screenshot 2023-04-30 at 01.31.39.png",
+      desc: "Carry your busines to a wider audience on the world wide web, make easy and smooth sales transactions.",
     },
     {
       id: "3",
-      service: "Landing page",
-      desc: "Custom websites to enhance your company's image.",
-    },
-    {
-      id: "4",
+      service: "Business website",
       img: "/igor-miske-Px3iBXV-4TU-unsplash.jpg",
-      link: "/design",
-    },
-    {
-      id: "5",
-      img: "/stephen-dawson-qwtCeJ5cLYs-unsplash.jpg",
-      link: "/mobileapp",
+      desc: "We create stunning, responsive websites that tell your story in a dramatic and visually appealing way.",
     },
     {
       id: "6",
       service: "SEO",
-      desc: "Stay top-of-mind with your audience with consistent digital designs.",
+      img: "/stephen-dawson-qwtCeJ5cLYs-unsplash.jpg",
+      desc: "Help new customers find your brand by improving your search engine rankings. Our SEO strategies will increase your website’s searchability, visibility, and relevance.",
       link: "/uiux",
     },
     {
       id: "3",
       service: "Site maintenace",
-      desc: "Custom websites to enhance your company's image.",
-    },
-    {
-      id: "4",
       img: "/Support_mockup.png",
-      link: "/design",
+      desc: "Our in-house web development team are experts at leveraging current trends and technologies to support your online presence and help you reach your goals.",
     },
+    // {
+    //   id: "4",
+    //   img: "/Support_mockup.png",
+    //   link: "/design",
+    // },
   ];
 
   return (
